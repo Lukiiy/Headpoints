07de059f080024afb460aaca3d28fff8cff8ad43
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2025-12-29 13:02:08 -0300

    oh... So there IS a PlayerFaceRenderer!
    * Reworked the majority of the code to use AvatarRenderer & PlayerFaceRenderer
    * Removed headpoints$head method since headpoints$pins now takes care of that

--- src/main/java/me/lukiiy/headpoints/mixin/HeadpinsMixin.java
+++ src/main/java/me/lukiiy/headpoints/mixin/HeadpinsMixin.java
@@ -4,7 +4,10 @@ import com.mojang.blaze3d.pipeline.RenderPipeline;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.ClientAvatarEntity;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.PlayerFaceRenderer;
 import net.minecraft.client.gui.contextualbar.LocatorBarRenderer;
+import net.minecraft.client.multiplayer.PlayerInfo;
+import net.minecraft.client.renderer.entity.player.AvatarRenderer;
 import net.minecraft.client.resources.WaypointStyle;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
@@ -23,24 +26,38 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+import java.util.UUID;
+
 @Mixin(LocatorBarRenderer.class)
 public class HeadpinsMixin {
     @Shadow @Final private Minecraft minecraft;
 
     @Redirect(method = "method_70870", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blitSprite(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/resources/ResourceLocation;IIIII)V"))
     private void headpoints$pins(GuiGraphics instance, RenderPipeline renderPipeline, ResourceLocation resourceLocation, int i, int j, int k, int l, int m, Entity entity, Level level, PartialTickSupplier pts, GuiGraphics instance2, int o, TrackedWaypoint t) {
-        Entity pinnedEntity = t.id().left().map(level::getEntity).orElse(null);
+        UUID id = t.id().left().orElse(null);
 
         PlayerSkin skin = null;
         boolean hat = true;
+        boolean upsideDown = false;
 
-        if (pinnedEntity instanceof Player player) {
-            skin = minecraft.getSkinManager().createLookup(player.getGameProfile(), true).get();
-            hat = player.isModelPartShown(PlayerModelPart.HAT);
+        if (id != null && minecraft.player != null) {
+            PlayerInfo info = minecraft.player.connection.getPlayerInfo(id);
+
+            if (info != null) {
+                skin = info.getSkin();
+                hat = info.showHat();
+            }
         }
-        if (pinnedEntity instanceof ClientAvatarEntity clientAvatar) {
-            skin = clientAvatar.getSkin();
-            hat = pinnedEntity instanceof Avatar avatar && avatar.isModelPartShown(PlayerModelPart.HAT);
+
+        if (skin == null || hat) {
+            Entity e = minecraft.level != null && id != null ? minecraft.level.getEntity(id) : null;
+
+            if (e instanceof Player player) {
+                upsideDown = AvatarRenderer.isPlayerUpsideDown(player);
+            } else if (skin == null && e instanceof ClientAvatarEntity clientAvatar) {
+                skin = clientAvatar.getSkin();
+                if (clientAvatar instanceof Avatar avatar) hat = avatar.isModelPartShown(PlayerModelPart.HAT);
+            }
         }
 
         if (skin == null) {
@@ -48,42 +65,24 @@ public class HeadpinsMixin {
             return;
         }
 
-        float dist = Math.min(pinnedEntity.distanceTo(minecraft.player), minecraft.options.renderDistance().get() * 16);
+        float dist = Mth.sqrt((float) t.distanceSquared(minecraft.getCameraEntity()));
+        int scale = Mth.ceil(8 * headpoints$scale(dist, minecraft.getWaypointStyles().get(t.icon().style)));
+        int renderX = (i + 4) - scale / 2;
+        int renderY = (j + 4) - scale / 2;
 
-        headpoints$head(instance, skin.body().texturePath(), i, j, 8, hat, headpoints$scale(dist, minecraft.getWaypointStyles().get(t.icon().style)));
+        PlayerFaceRenderer.draw(instance, skin.body().texturePath(), renderX, renderY, scale, hat, upsideDown, -1);
     }
 
     @Unique
     private static float headpoints$scale(float dist, WaypointStyle style) {
-        float nearDist = style.nearDistance();
-        float farDist = style.farDistance();
+        float near = style.nearDistance();
+        float far = style.farDistance();
 
-        if (dist <= nearDist) return 1;
-        if (dist >= farDist) return .5f;
+        if (dist <= near) return 1;
+        if (dist >= far) return .5f;
 
-        float normalized = (dist - nearDist) / (farDist - nearDist);
+        float normalized = (dist - near) / (far - near);
 
         return Mth.lerp(normalized, 1, .5f);
-    }
-
-    @Unique
-    private void headpoints$head(GuiGraphics instance, ResourceLocation texture, int x, int y, int base, boolean hat, float scale) {
-        int scaledSize = Mth.ceil(base * scale);
-        int fX = (x + base / 2) - scaledSize / 2;
-        int fY = (y + base / 2) - scaledSize / 2;
-
-        float uMin = 8f / 64f;
-        float uMax = 16f / 64f;
-        float vMin = 8f / 64f;
-        float vMax = 16f / 64f;
-
-        instance.blit(texture, fX, fY, fX + scaledSize, fY + scaledSize, uMin, uMax, vMin, vMax);
-
-        if (hat) {
-            float hatUMin = 40f / 64f;
-            float hatUMax = 48f / 64f;
-
-            instance.blit(texture, fX, fY, fX + scaledSize, fY + scaledSize, hatUMin, hatUMax, vMin, vMax);
-        }
     }
 }
