20e1fddf9acc64ba7b8c735bf627525fb44ebb73
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2025-12-29 07:32:14 -0300

    few more tweaks
    * Added a scale function to rescale the shown skin
    * Tweaked the head display function a bit
    * Added support for Mannequins/Avatar entities

--- src/main/java/me/lukiiy/headpoints/mixin/HeadpinsMixin.java
+++ src/main/java/me/lukiiy/headpoints/mixin/HeadpinsMixin.java
@@ -2,9 +2,13 @@ package me.lukiiy.headpoints.mixin;
 
 import com.mojang.blaze3d.pipeline.RenderPipeline;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.ClientAvatarEntity;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.contextualbar.LocatorBarRenderer;
+import net.minecraft.client.resources.WaypointStyle;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.Avatar;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.player.PlayerModelPart;
@@ -25,31 +29,61 @@ public class HeadpinsMixin {
 
     @Redirect(method = "method_70870", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blitSprite(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/resources/ResourceLocation;IIIII)V"))
     private void headpoints$pins(GuiGraphics instance, RenderPipeline renderPipeline, ResourceLocation resourceLocation, int i, int j, int k, int l, int m, Entity entity, Level level, PartialTickSupplier pts, GuiGraphics instance2, int o, TrackedWaypoint t) {
-        Player player = t.id().left().map(level::getPlayerByUUID).orElse(null);
-        if (player == null) {
+        Entity pinnedEntity = t.id().left().map(level::getEntity).orElse(null);
+
+        PlayerSkin skin = null;
+        boolean hat = true;
+
+        if (pinnedEntity instanceof Player player) {
+            skin = minecraft.getSkinManager().createLookup(player.getGameProfile(), true).get();
+            hat = player.isModelPartShown(PlayerModelPart.HAT);
+        }
+        if (pinnedEntity instanceof ClientAvatarEntity clientAvatar) {
+            skin = clientAvatar.getSkin();
+            hat = pinnedEntity instanceof Avatar avatar && avatar.isModelPartShown(PlayerModelPart.HAT);
+        }
+
+        if (skin == null) {
             instance.blitSprite(renderPipeline, resourceLocation, i, j, k, l, m);
             return;
         }
 
-        PlayerSkin skin = minecraft.getSkinManager().createLookup(player.getGameProfile(), true).get();
+        float dist = Math.min(pinnedEntity.distanceTo(minecraft.player), minecraft.options.renderDistance().get() * 16);
 
-        headpoints$head(instance, skin.body().texturePath(), i, j, 8, player);
+        headpoints$head(instance, skin.body().texturePath(), i, j, 8, hat, headpoints$scale(dist, minecraft.getWaypointStyles().get(t.icon().style)));
     }
 
     @Unique
-    private void headpoints$head(GuiGraphics instance, ResourceLocation tex, int x, int y, int s, Player player) {
+    private static float headpoints$scale(float dist, WaypointStyle style) {
+        float nearDist = style.nearDistance();
+        float farDist = style.farDistance();
+
+        if (dist <= nearDist) return 1;
+        if (dist >= farDist) return .5f;
+
+        float normalized = (dist - nearDist) / (farDist - nearDist);
+
+        return Mth.lerp(normalized, 1, .5f);
+    }
+
+    @Unique
+    private void headpoints$head(GuiGraphics instance, ResourceLocation texture, int x, int y, int base, boolean hat, float scale) {
+        int scaledSize = Mth.ceil(base * scale);
+        int fX = (x + base / 2) - scaledSize / 2;
+        int fY = (y + base / 2) - scaledSize / 2;
+
         float uMin = 8f / 64f;
         float uMax = 16f / 64f;
         float vMin = 8f / 64f;
         float vMax = 16f / 64f;
 
-        instance.blit(tex, x, y, x + s, y + s, uMin, uMax, vMin, vMax);
+        instance.blit(texture, fX, fY, fX + scaledSize, fY + scaledSize, uMin, uMax, vMin, vMax);
 
-        if (player.isModelPartShown(PlayerModelPart.HAT)) {
-            float huMin = 40f / 64f;
-            float huMax = 48f / 64f;
+        if (hat) {
+            float hatUMin = 40f / 64f;
+            float hatUMax = 48f / 64f;
 
-            instance.blit(tex, x, y, x + s, y + s, huMin, huMax, vMin, vMax);
+            instance.blit(texture, fX, fY, fX + scaledSize, fY + scaledSize, hatUMin, hatUMax, vMin, vMax);
         }
     }
 }
